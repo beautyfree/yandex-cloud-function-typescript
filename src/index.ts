9e51b71dc905d68e8b5c6c8c952d199bb58be3a0
Alexey Elizarov <felex-ae@ya.ru>
2024-09-16 01:02:08 +0300

    feat: use exists types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { Handler } from './typings';
+import { Handler } from '@yandex-cloud/function-types'
 
-export const handler: Handler = (event, context) => {
-  console.log('Hello world!');
+export const handler: Handler.Http = (event, context) => {
+  console.log('Hello world!')
 
   return {
     statusCode: 200,
     body: JSON.stringify({ event, context }),
-  };
-};
+  }
+}
